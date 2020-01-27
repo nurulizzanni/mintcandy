c493adf217a21581af97fb19847e5fd0ac2b06fe
nurulizzanni <57710826+nurulizzanni@users.noreply.github.com>
2020-01-27 21:48:17 +0800

    Update register.blade.php

--- resources/views/site/register.blade.php
+++ resources/views/site/register.blade.php
@@ -20,7 +20,7 @@
 			</section>
 
             <section class="search-course-area relative" style="background:unset;">
-				<div class="overlay overlay-bg"></div>
+
 				<div class="container">
 					<div class="row justify-content-between align-items-center">
 						<div class="col-lg-6 col-md-6 search-course-left">
@@ -35,7 +35,7 @@
 						</div>
 						<div class="col-lg-4 col-md-6 search-course-right section-gap">
 							<form class="form-wrap" action="#">
-
+								<h4 class="text-white pb-20 text-center mb-30">Search for Available Course</h4>		
 								<input type="text" class="form-control" name="name" placeholder="Your Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Name'">
 								<input type="phone" class="form-control" name="phone" placeholder="Your Phone Number" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Phone Number'">
 								<input type="email" class="form-control" name="email" placeholder="Your Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Email Address'">
