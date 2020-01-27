d0614c4ae156b6e61f12b7110ec9d5a68ecb5fb7
nurulizzanni <57710826+nurulizzanni@users.noreply.github.com>
2020-01-27 21:38:49 +0800

    Update register.blade.php

--- resources/views/site/register.blade.php
+++ resources/views/site/register.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.frontend')
 
 @section('content')
-
 <section class="banner-area relative" id="home">
 				<div class="overlay overlay-bg"></div>	
 				<div class="container">
 					<div class="row fullscreen d-flex align-items-center justify-content-between" style="height: 574px;">
 						<div class="banner-content col-lg-9 col-md-12">
 							<h1 class="text-uppercase">
-									Register		
+								We Ensure better education
+								for a better world			
 							</h1>
 							<p class="pt-10 pb-10">
-								Register
+								In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space telescope known as the Hubble.
 							</p>
 							<a href="#" class="primary-btn text-uppercase">Get Started</a>
 						</div>										
@@ -19,23 +19,38 @@
 				</div>					
 			</section>
 
-
-            <section class="search-course-area relative" style="background: unset;">
-			
+            <section class="search-course-area relative">
+				<div class="overlay overlay-bg"></div>
 				<div class="container">
 					<div class="row justify-content-between align-items-center">
 						<div class="col-lg-6 col-md-6 search-course-left">
-							<h1 >
+							<h1 class="text-white">
 								Get reduced fee <br>
 								during this Summer!
 							</h1>
 							<p>
 								inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that, as women, our behavior on the job is beyond reproach.
 							</p>
-							
+							<div class="row details-content">
+								<div class="col single-detials">
+									<span class="lnr lnr-graduation-hat"></span>
+									<a href="#"><h4>Expert Instructors</h4></a>		
+									<p>
+										Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+									</p>						
+								</div>
+								<div class="col single-detials">
+									<span class="lnr lnr-license"></span>
+									<a href="#"><h4>Certification</h4></a>		
+									<p>
+										Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+									</p>						
+								</div>								
+							</div>
+						</div>
 						<div class="col-lg-4 col-md-6 search-course-right section-gap">
 							<form class="form-wrap" action="#">
-								<h4  pb-20 text-center mb-30">Search for Available Course</h4>		
+								<h4 class="text-white pb-20 text-center mb-30">Search for Available Course</h4>		
 								<input type="text" class="form-control" name="name" placeholder="Your Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Name'">
 								<input type="phone" class="form-control" name="phone" placeholder="Your Phone Number" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Phone Number'">
 								<input type="email" class="form-control" name="email" placeholder="Your Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Email Address'">
@@ -54,4 +69,5 @@
 					</div>
 				</div>	
 			</section>
+
 @stop
