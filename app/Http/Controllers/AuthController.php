aefc133694ee816ae28b1ce1787cfe17abb84dd2
nurulizzanni <57710826+nurulizzanni@users.noreply.github.com>
2020-01-31 07:41:31 +0800

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
     public function postlogin(Request $request)
     {
         if(Auth::attempt($request->only('email','password'))){
-            return redirect('/dashboard');
+            return redirect('/dass1');
 
         }
         return redirect('/login');
