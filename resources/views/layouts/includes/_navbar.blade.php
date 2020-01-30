335995ace300f0202c2c2e646dea548b417479fb
nurulizzanni <57710826+nurulizzanni@users.noreply.github.com>
2020-01-31 07:36:05 +0800

    navbar

--- resources/views/layouts/includes/_navbar.blade.php
+++ resources/views/layouts/includes/_navbar.blade.php
@@ -20,7 +20,7 @@
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="{{secure_asset('/frontend')}}/img/love.png" class="img-circle" alt="Avatar"> <span>{{auth()->user()->name}}</span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
 							<ul class="dropdown-menu">
-								<li><a href="#"><i class="lnr lnr-user"></i> <span>My Profile</span></a></li>
+			
 								<li><a href="/logout"><i class="lnr lnr-exit"></i> <span>Logout</span></a></li>
 							</ul>
 						</li>
